7b74b51d5a61aecf952bc597091779a274f2b5b9
TatyanaZakiryanova <elenastrell@yandex.ru>
2025-07-19 17:52:43 +0300

    add merge sorted array solution

---
+++ array/mergeSortedArray.js
@@ -0,0 +1,24 @@
+// Merge sorted array
+
+// https://leetcode.com/problems/merge-sorted-array/
+// easy
+
+function merge(nums1, m, nums2, n) {
+  let p1 = m - 1;
+  let p2 = n - 1;
+  let p = m + n - 1;
+
+  while (p2 >= 0) {
+    if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+      nums1[p] = nums1[p1];
+      p1--;
+    } else {
+      nums1[p] = nums2[p2];
+      p2--;
+    }
+    p--;
+  }
+}
+
+// Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+// Output: [1,2,2,3,5,6]
